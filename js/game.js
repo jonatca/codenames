fdddc51103c74a6af8315b885c9e1476ac2e9c5e
Jonathan Carlson <jonathan.carlson01@outlook.com>
2023-01-17 15:57:55 +0100

    Made code easier to read

--- js/game.js
+++ js/game.js
@@ -3,49 +3,10 @@ export default class Game {
   constructor() {
     this.cards = new Cards();
     this.turn = this.cards.getStartingTeam();
-    this.paintTurn();
+    this.paintTurn(); //paints the startingturn backround of numbergrid
     this.numberGuesses = 0;
   }
-  cardClicked(rectangle, i) {
-    if (!this.cards.isShowing(i) && this.numberGuesses > 0) {
-      //else missclick
-      rectangle[i].style.backgroundColor = this.cards.getColor(i);
-      this.cards.setShowing(i);
-      this.cards.reduceCardsLeft(i);
-      this.changeGuessesAndTurn(i);
-      this.paintTurn();
-      this.checkGameOver();
-    }
-  }
-  paintTurn() {
-    const numberGrid = document.getElementById("number-grid");
-    numberGrid.style.backgroundColor = this.turn;
-  }
 
-  changeGuessesAndTurn(i) {
-    this.numberGuesses -= 1;
-    //if with or statement
-
-    if (this.numberGuesses == 0 || this.cards.getColor(i) != this.turn) {
-      //if guess is wrong or out of guesses
-      if (this.turn == "red") {
-        this.turn = "blue";
-      } else {
-        this.turn = "red";
-      }
-      this.resetBoxesGrey();
-      this.numberGuesses = 0;
-      //   let numberGrid = document.getElementById("number-grid");
-      //   numberGrid.style.backgroundColor = this.turn;
-    }
-  }
-  resetBoxesGrey() {
-    for (let j = 1; j <= 6; j++) {
-      //reset number boxes to grey
-      var numberBox = document.getElementsByClassName("number")[j - 1];
-      numberBox.style.backgroundColor = "#f2f2f2";
-    }
-  }
   numberBoxClicked(i) {
     if (this.numberGuesses == 0) {
       //only change if out of guesses, else missclick
@@ -55,11 +16,50 @@ export default class Game {
     }
     this.numberGuesses = i;
   }
+
+  cardClicked(rectangle, i) {
+    if (!this.cards.isShowing(i) && this.numberGuesses > 0) {
+      //not missclick
+      rectangle[i].style.backgroundColor = this.cards.getColor(i); //change color of the card
+      this.cards.setShowing(i); //set card to showing = True (not needed?)
+      this.cards.reduceCardsLeft(i);
+      this.changeGuessesAndTurn(i);
+      this.paintTurn(); //paints the turn backround of numbergrid
+      this.checkGameOver();
+    }
+  }
+
+  changeGuessesAndTurn(i) {
+    this.numberGuesses -= 1;
+    if (this.numberGuesses == 0 || this.cards.getColor(i) != this.turn) {
+      //if guess is wrong or out of guesses
+      if (this.turn == "red") {
+        this.turn = "blue";
+      } else {
+        this.turn = "red";
+      }
+      this._resetBoxesGrey();
+      this.numberGuesses = 0;
+    }
+  }
+
+  paintTurn() {
+    const numberGrid = document.getElementById("number-grid");
+    numberGrid.style.backgroundColor = this.turn;
+  }
+
   checkGameOver() {
     if (this.cards.getRedCardsLeft() == 0) {
       console.log("red wins");
     } else if (this.cards.getBlueCardsLeft() == 0) {
       console.log("blue wins");
+    }
+  }
+  _resetBoxesGrey() {
+    for (let j = 1; j <= 6; j++) {
+      //reset number boxes to grey
+      var numberBox = document.getElementsByClassName("number")[j - 1];
+      numberBox.style.backgroundColor = "#f2f2f2";
     }
   }
 }
